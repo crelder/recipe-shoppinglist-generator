e584ae9b0592755ae71705484fd1f417b93e2ad9
crelder <crelder@github.com>
2018-07-04 19:51:03 +0200

    Departmens added

--- recipes.js
+++ recipes.js
@@ -9,34 +9,13 @@ xmlhttp.onreadystatechange = function() {
 xmlhttp.open("GET", "https://rawgit.com/crelder/recipe-shoppinglist-generator/develop/recipes.json", false);
 xmlhttp.send();
 // Use the hereby added recipe.selected Property to select/unselect recipes in the GUI and to define the shopping list.
-x.forEach(function(recipe) {
+x.forEach(function(recipe, index) {
     recipe.selected = false;
-});
-// Randem order of recipes
-const recipeCollection = x.sort(function(a, b) {
-    return 0.5 - Math.random()
-});
-console.log("recipeCollection:", recipeCollection);
-/* --------------------------- */
 
-/* ---- Function: removes duplicates from shoppingList and accumulates ingredients -- */
-Array.prototype.unique = function() {
-    var a = this;
-    console.log("a", a);
-    for (var i = 0; i < a.length; ++i) {
-        for (var j = i + 1; j < a.length; ++j) {
-            if (a[i].name === a[j].name && a[i].unit === a[j].unit) {
-                console.log(a[i].name, a[j].name);
-                console.log("a: ", a);
-                console.log("a[i].amount += a[j].amount", a[i].amount, a[j].amount);
-                a[i].amount += a[j].amount;
-                a.splice(j--, 1);
-            }
-        }
-    }
-    return a;
-};
-/* ----------------------- */
+});
+// Random order of recipes
+const recipeCollection = x.sort(function() {return 0.5 - Math.random()});
+/* --------------------------- */
 
 Vue.component('my-meal', {
     props: ['recipe', 'recipes', 'index'],
@@ -45,8 +24,8 @@ Vue.component('my-meal', {
             this.recipes[this.index].selected = !this.recipes[this.index].selected;
         }
     },
-    template: '<a href="#" class="list-group-item list-group-item-action" v-bind:class="{active: recipes[index].selected}" v-on:click="toggleSelectedRecipe"> {{ recipe.recipeName }}</a>',
-})
+    template: '<a href="javascript:void(0);" class="list-group-item list-group-item-action" v-bind:class="{active: recipes[index].selected}" v-on:click="toggleSelectedRecipe"> {{ recipe.recipeName }}</a>',
+});
 
 var vm = new Vue({
     el: '#app',
@@ -55,29 +34,56 @@ var vm = new Vue({
     },
     methods: {
         onCopy: function(e) {
-            alert('You just copied: ' + e.text)
+            alert('Folgende Einkaufsliste ist in die Zwischenablage kopiert:\n' + e.text)
         },
         onError: function(e) {
-            alert('Failed to copy texts')
+            alert('Fehler beim Kopieren in die Zwischenablage.')
+        },
+        accumulateRecipes: function(a){
+            console.log("var a = array;", a);
+            console.log("recipes", this.recipes);
+            for (var i = 0; i < a.length; ++i) {
+                console.log("i", a[i].amount);
+                for (var j = i + 1; j < a.length; ++j) {
+                    console.log("i", a[j].amount);
+                    if (a[i].name === a[j].name && a[i].unit === a[j].unit) {
+                        a[i].amount += a[j].amount;
+                        console.log("a.splice(j, 1);", a.splice(j, 1));
+                        a.splice(j--, 1);
+                    }
+                }
+            }
+            return a;
+        },
+        sortIngredientsByDepartment: function(a) {
+            console.log(a);
+            let departments = a.map(ingredient => ingredient.department);
+            console.log(departments);
         }
     },
     computed: {
         shoppingList: function() {
-            console.log("shoppingList");
-            let vm = this;
             let shoppingList = [];
-            let x = vm.recipes.filter(recipe => recipe.selected == true).map(recipe => recipe.ingredients);
+            let recipes = this.recipes;
+            // Create an array with all the ingredients of the selected recipes
+            let x = recipes.filter(recipe => recipe.selected == true).map(recipe => recipe.ingredients);
             shoppingList = [].concat.apply([], x);
-            console.log("shoppingList", shoppingList);
-            console.log("shoppingList.unique()", shoppingList.unique());
+            // Accumulate similar ingredients
+            shoppingList = this.accumulateRecipes(shoppingList);
+            if (shoppingList.length > 0) {
+                shoppingList = this.sortIngredientsByDepartment(shoppingList);
+            }
+            // Create an array with one string for each ingredient
             shoppingList = shoppingList.map(ingredient => ingredient.amount + " " + ingredient.unit + " " + ingredient.name);
+            
             console.log("shoppingList", shoppingList);
+
             return shoppingList;
         },
         clipboardShoppingList: function() {
             return this.shoppingList.join("\n");
         },
-        selectedRecipes: function(shoppingList) {
+        selectedRecipes: function() {
             return this.recipes.filter(recipe => recipe.selected == true);
         }
     }
